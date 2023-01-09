12d6c657a0a480ce87f0ce5cce38ac9339450814
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-09 20:25:22 +0400

    delete and update post

--- src/views/DetailPostView.vue
+++ src/views/DetailPostView.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="post">
+    <modal-create-and-edit
+        v-if="stateModalWindowCreateOrEditPost"
+        @click="toggleModalWindowCreateOrEditPost"
+        v-model:post="post"
+    >
+      <main-button @click="editPost">Сохранить</main-button>
+    </modal-create-and-edit>
     <h2>{{ post.title }}</h2>
     <p class="post__descriptions">{{ post.description }}</p>
     <p class="post__created">created {{ post.createdAt }}</p>
     <p class="post__updated">updated {{ post.updatedAt }}</p>
+    <main-button class="button" @click="toggleModalWindowCreateOrEditPost">Изменить</main-button>
+    <main-button @click="deletePost">Удалить</main-button>
   </div>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-import {GET_POST} from "@/const/const.request-server";
+import {mapActions, mapGetters} from "vuex";
+import {DELETE_POST, GET_POST, UPDATE_POST} from "@/const/const.request-server";
 import Post from "@/components/Post.vue";
+import MainButton from "@/components/UI/MainButton.vue";
+import ModalCreateAndEdit from "@/components/ModalCreateAndEdit.vue";
 
 export default {
   name: "DetailPostView",
-  components: {Post},
+  components: {ModalCreateAndEdit, MainButton, Post},
   data() {
     return {
       post: {
@@ -27,6 +38,7 @@ export default {
     }
   },
   computed: {
+    ...mapGetters('storeCreateOrEditPost', ['stateModalWindowCreateOrEditPost']),
     ...mapGetters('requestServer', ['requestServerPost'])
   },
   mounted() {
@@ -38,7 +50,25 @@ export default {
           this.post.updatedAt = this.post.updatedAt.split('T')[0];
         })
   },
-  methods: {}
+  methods: {
+    ...mapActions('storeCreateOrEditPost', ['toggleModalWindowCreateOrEditPost']),
+    async editPost() {
+      const bodyPost = {id: this.post._id, title: this.post.title, description: this.post.description};
+      await this.requestServerPost(UPDATE_POST, bodyPost);
+      await this.requestServerPost(GET_POST, {id: bodyPost.id})
+          .then((post) => {
+            this.post = post.data
+            this.post.createdAt = this.post.createdAt.split('T')[0];
+            this.post.updatedAt = this.post.updatedAt.split('T')[0];
+          })
+      this.toggleModalWindowCreateOrEditPost();
+    },
+    deletePost() {
+      const bodyPost = {id: this.post._id};
+      this.requestServerPost(DELETE_POST, bodyPost)
+          .then(()=> window.history.back())
+    }
+  }
 }
 </script>
 
@@ -66,5 +96,9 @@ export default {
   right: 20px;
   bottom: 0;
   font-size: 10px;
+}
+
+.button {
+  margin-right: 10px;
 }
 </style>
