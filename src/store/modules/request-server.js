3b5b23d05d48ba7e18067cefe2bdbf8faffd4a5e
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-10 18:48:53 +0400

    fix request create user

--- src/store/modules/request-server.js
+++ src/store/modules/request-server.js
@@ -1,16 +1,10 @@
 import axios from "axios";
-import {HEADER_REQUEST, CREATE_USER, BURL, HEADER_CONTENT_TYPE} from "@/const/const.request-server";
+import {BURL, HEADER_CONTENT_TYPE} from "@/const/const.request-server";
 import {TOKEN} from "@/const/const";
 
 const state = () => ({})
 
 const getters = {
-    requestCreateUser: () => (params) => {
-        return axios.post(BURL + CREATE_USER, {
-            login: params.login,
-            password: params.password
-        }, {headers: HEADER_REQUEST})
-    },
     requestServerPost: () => (urlBody, body) => {
         const token = localStorage.getItem(TOKEN);
         return axios.post(BURL + urlBody, body, {
