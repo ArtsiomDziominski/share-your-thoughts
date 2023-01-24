98005ef6fbe7b2d9256d20ce9cc8c9b2bc727d1d
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-24 16:04:49 +0400

    create component AuthorDatePost.vue and change the location of the author and date of the post

---
+++ src/components/AuthorDatePost.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="author-date">
+    <p class="author-date__author">{{ post.author }}</p>
+    <p class="author-date__created">created {{ post.createdAt }}</p>
+    <p class="author-date__updated">updated {{ post.updatedAt }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthorDatePost",
+  props: {
+    post: {
+      title: String,
+      description: String,
+      updatedAt: String,
+      author: String,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.author-date p {
+  margin: 0;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.author-date__created {
+  font-size: 10px;
+  color: var(--color-post-data-update);
+}
+
+.author-date__updated {
+  font-size: 10px;
+  color: var(--color-post-data-update);
+}
+
+.author-date__author {
+  font-size: 10px;
+  color: var(--color-post-data-update);
+}
+</style>
