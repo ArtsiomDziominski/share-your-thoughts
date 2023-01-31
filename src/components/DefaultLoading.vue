bdc7c6e2ea6b6876d23333558c7c057520873ddf
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-31 21:10:34 +0400

    save token store

--- src/components/DefaultLoading.vue
+++ src/components/DefaultLoading.vue
@@ -6,6 +6,7 @@ import {onMounted} from "vue";
 import {useStore} from "vuex";
 import {getAllPost} from "@/helpers/get-all-posts";
 import {getUser} from "@/helpers/get-user";
+import {TOKEN} from "@/const/const";
 
 export default {
   name: "DefaultLoading",
@@ -15,8 +16,10 @@ export default {
     onMounted(async () => {
       const post = await getAllPost();
       const user = await getUser()
+      const token = localStorage.getItem(TOKEN)
 
       await store.dispatch('storeUser/setUserInfo', user);
+      await store.dispatch('storeUser/setUserToken', token);
       await store.dispatch('storeAllPosts/updateAllPosts', post);
       await store.dispatch('storeUser/toggleActiveUser', true);
     })
