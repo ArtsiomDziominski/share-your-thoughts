a6e452afbf8f3d7897de448c5eb2143510d0ef38
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-31 17:35:20 +0400

    default download file

---
+++ src/components/DefaultLoading.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script>
+import {onMounted} from "vue";
+import {getAllPost} from "@/hooks/get-all-posts";
+import {getCurrentUser} from "@/hooks/get-current-user";
+import {useStore} from "vuex";
+
+export default {
+  name: "DefaultLoading",
+  setup() {
+    const store = useStore();
+
+    onMounted(async () => {
+      const post = await getAllPost();
+      const user = await getCurrentUser()
+
+      await store.dispatch('storeUser/setUserInfo', user);
+      await store.dispatch('storeAllPosts/updateAllPosts', post);
+      await store.dispatch('storeUser/toggleActiveUser', true);
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
